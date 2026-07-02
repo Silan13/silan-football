245268f7a689dc4b89040ad6aab1c1b54770f21a
Silan13 <samismgame@gmail.com>
2026-07-02 23:03:32 +0330

    Implement user login functionality in auth.php

--- api/auth.php
+++ api/auth.php
@@ -1,1 +1,25 @@
+<?php
+require "db.php";
+session_start();
 
+$data = json_decode(file_get_contents("php://input"), true);
+
+if ($data["action"] == "login") {
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username=?");
+    $stmt->execute([$data["username"]]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($data["password"], $user["password"])) {
+
+        echo json_encode([
+            "status" => "ok",
+            "user_id" => $user["id"],
+            "nickname" => $user["nickname"]
+        ]);
+
+    } else {
+        echo json_encode(["status" => "error"]);
+    }
+}
+?>
