de51071dffb71218a35d2d50cfa82fbdf29ff1ae
Silan13 <samismgame@gmail.com>
2026-07-02 21:38:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Silan Football Started");
+
+document.querySelector(".login-btn").onclick=function(){
+
+alert("سیستم ورود هنوز ساخته نشده :)");
+
+}
+
+document.querySelector(".register-btn").onclick=function(){
+
+alert("صفحه ثبت نام در جلسه بعد ساخته می‌شود.");
+
+}
